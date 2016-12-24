c37a1c4f2e4ce22b1d9375bcb0263240c71eb441
rig0rmortis <admin@rig0rmortis.com>
2016-12-24 15:51:30 -0500

    Naive and slow solution for 3

--- 3.go
+++ 3.go
@@ -6,27 +6,27 @@ What is the largest prime factor of the number 600,851,475,143 ?
 
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
 func main() {
-	fmt.Println(computeMaxPrimeFactor(600851475143))
+	num := int64(600851475143)
+	fmt.Println(computeMaxPrimeFactor(num))
 }
 
-func computeMaxPrimeFactor(n int) int {
-	primeFactors := []int{}
+func computeMaxPrimeFactor(n int64) int64 {
+	primeFactors := []int64{}
 	for _, factor := range getFactors(n) {
+		fmt.Printf("Working on factor: %d", factor)
 		if isPrime(factor) {
 			primeFactors = append(primeFactors, factor)
 		}
 	}
-	return sort.IntSlice(primeFactors)[len(primeFactors)-1]
+	return maxNum(primeFactors)
 }
 
-func isPrime(n int) bool {
-	for i := 2; i < n/2; i++ {
+func isPrime(n int64) bool {
+	fmt.Printf("Checking if %d is prime...", n)
+	for i := int64(2); i < n/2; i++ {
 		if n%i == 0 {
 			return false
 		}
@@ -34,12 +34,24 @@ func isPrime(n int) bool {
 	return true
 }
 
-func getFactors(n int) []int {
-	factors := []int{}
-	for i := 1; i <= n; i++ {
+func getFactors(n int64) []int64 {
+	fmt.Printf("Getting factors for %d", n)
+	factors := []int64{1, n}
+	for i := int64(2); i <= n/2; i++ {
 		if n%i == 0 {
 			factors = append(factors, i)
 		}
 	}
 	return factors
+}
+
+func maxNum(nums []int64) int64 {
+	fmt.Printf("Getting the max number")
+	max := int64(1)
+	for _, num := range nums {
+		if num > max {
+			max = num
+		}
+	}
+	return max
 }
